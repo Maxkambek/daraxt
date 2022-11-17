c7138f8ca39402225cca4204703c2c3ff5e693db
Maxkambek <mahkamkasimov94@gmail.com>
2022-11-17 16:18:12 +0500

    a

--- reports/urls.py
+++ reports/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('report/', views.TreeReportCreateAPIView.as_view()),
-    path('docs/', views.DocsAPIView.as_view())
+    path('docs/', views.DocsAPIView.as_view()),
+    path('docs-status/', views.DocsUpdateAPIView.as_view())
 ]
